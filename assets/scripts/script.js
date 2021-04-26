bc8589219d504f49e7eecfb4a078d2b4b0c4655c
victoriaye1029 <victoriaye@berkeley.edu>
2021-04-26 15:01:55 -0700

    started a website

---
+++ assets/scripts/script.js
@@ -0,0 +1,85 @@
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides1 = document.getElementsByClassName("mySlides1");
+  if (n > slides1.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides1.length}
+    for (i = 0; i < slides1.length; i++) {
+      slides1[i].style.display = "none";
+    }
+  slides1[slideIndex1-1].style.display = "block";
+}
+
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  if (n > slides2.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides2.length}
+    for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";
+    }
+  slides2[slideIndex2-1].style.display = "block";
+}
+
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides3 = document.getElementsByClassName("mySlides3");
+  if (n > slides3.length) {slideIndex3 = 1}
+    if (n < 1) {slideIndex3 = slides3.length}
+    for (i = 0; i < slides3.length; i++) {
+      slides3[i].style.display = "none";
+    }
+  slides3[slideIndex3-1].style.display = "block";
+}
+
+
+
+const links = document.querySelectorAll("#container1 button");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
